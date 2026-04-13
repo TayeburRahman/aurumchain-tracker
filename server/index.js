18ccb3fda8e8394e316feb87d896b8647f9008cd
TayeburRahman <tayebrayhan101+@gmail.com>
2026-04-13 15:47:25 +0600

    updates all

--- server/index.js
+++ server/index.js
@@ -270,6 +270,10 @@ app.post('/api/tasks/delete', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
